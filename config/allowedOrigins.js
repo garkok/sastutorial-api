82c2c5f8bc044782bc744e0f37c4e1b9691f098c
unknown <alparb@yahoo.com>
2022-10-04 03:54:41 +0300

    Ready to deploy backend

---
+++ config/allowedOrigins.js
@@ -0,0 +1,8 @@
+const allowedOrigins = [
+    /*'http://localhost:3000',
+    'https://www.dandrepairshop.com',
+    'https://dandrepairshop.com',*/
+    'https://sastutorial.onrender.com/'
+]
+
+module.exports = allowedOrigins
