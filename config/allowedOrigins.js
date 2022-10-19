726ad5b062562c095b0e2d57a879818af85f0e10
unknown <alparb@yahoo.com>
2022-10-19 17:58:10 +0300

    settings change for CORS

--- config/allowedOrigins.js
+++ config/allowedOrigins.js
@@ -2,7 +2,8 @@ const allowedOrigins = [
     /*'http://localhost:3000',
     'https://www.dandrepairshop.com',
     'https://dandrepairshop.com',*/
-    'https://sastutorial.onrender.com/'
+    'https://sastutorial.onrender.com/',
+    'https://sastutorial-api.onrender.com/'
 ]
 
 module.exports = allowedOrigins
